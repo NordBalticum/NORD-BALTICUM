343a87e9569abbfd1b1a84507e64790b610b5d39
NordBalticum <info@litexus.lt>
2025-03-08 10:20:59 +0200

    Update ThemeSwitcher.js

--- src/components/ThemeSwitcher.js
+++ src/components/ThemeSwitcher.js
@@ -1,16 +1,26 @@
-import React, { useContext } from "react";
-import { ThemeContext } from "../context/ThemeContext";
+import React, { useState, useEffect } from "react";
 import { FaSun, FaMoon } from "react-icons/fa";
 import "../styles/themeSwitcher.css";
 
-const ThemeSwitcher = () => {
-  const { theme, toggleTheme } = useContext(ThemeContext);
+export default function ThemeSwitcher() {
+  const [theme, setTheme] = useState("dark");
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme") || "dark";
+    setTheme(savedTheme);
+    document.documentElement.setAttribute("data-theme", savedTheme);
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = theme === "dark" ? "light" : "dark";
+    setTheme(newTheme);
+    document.documentElement.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
+  };
 
   return (
     <button className="theme-switcher" onClick={toggleTheme}>
-      {theme === "light" ? <FaMoon /> : <FaSun />}
+      {theme === "dark" ? <FaSun className="icon" /> : <FaMoon className="icon" />}
     </button>
   );
-};
-
-export default ThemeSwitcher;
+}
