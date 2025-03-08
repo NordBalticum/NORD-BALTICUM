2dea60c31a95b4786646aa78d3c26f2dac8c0a1e
NordBalticum <info@litexus.lt>
2025-03-08 07:46:52 +0200

    Update and rename src/next.config.json to next.config.js

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    NEXT_PUBLIC_BSC_SCAN_API: process.env.NEXT_PUBLIC_BSC_SCAN_API,
+    NEXT_PUBLIC_REOWN_PROJECT_ID: process.env.NEXT_PUBLIC_REOWN_PROJECT_ID
+  }
+};
+
+module.exports = nextConfig;
