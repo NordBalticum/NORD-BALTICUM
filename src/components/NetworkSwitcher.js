38197cfa25b85ef4089c0c640700dd6cbf375f55
NordBalticum <info@litexus.lt>
2025-03-08 22:54:01 +0200

    Update NetworkSwitcher.js

--- src/components/NetworkSwitcher.js
+++ src/components/NetworkSwitcher.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import styles from "@/styles/networkswitcher.module.css";
 
 const NetworkSwitcher = () => {
   const [network, setNetwork] = useState("Mainnet");
