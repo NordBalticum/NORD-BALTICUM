80a8f23b4d3b3fe8838eb5d158ee6bf2dc1b105b
NordBalticum <info@litexus.lt>
2025-03-08 09:54:31 +0200

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© 2025 NORD BALTICUM. All Rights Reserved.</p>
+      <p className="tagline">The Future of Web3 Finance.</p>
+    </footer>
+  );
+};
+
+export default Footer;
