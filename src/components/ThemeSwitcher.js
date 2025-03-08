483f762f224a54f8c24938903928f7a669f42bfe
NordBalticum <info@litexus.lt>
2025-03-08 05:53:01 +0200

    Create ThemeSwitcher.js

---
+++ src/components/ThemeSwitcher.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+export default function ThemeSwitcher() {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme) {
+      setTheme(storedTheme);
+      document.documentElement.setAttribute("data-theme", storedTheme);
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+    document.documentElement.setAttribute("data-theme", newTheme);
+  };
+
+  return <button onClick={toggleTheme}>Switch Theme</button>;
+}
