26def7f9db40d1c3fc623cc2938443eb7868d4c7
NordBalticum <info@litexus.lt>
2025-03-08 10:47:54 +0200

    Update Buttons.js

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,21 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "../styles/buttons.css";
+import "../styles/Buttons.css";
 
-export const Button = ({ type = "primary", onClick, children, fullWidth }) => {
+export const Button = ({ type = "primary", onClick, children, icon, fullWidth }) => {
   return (
     <button
-      className={`button ${type} ${fullWidth ? "full-width" : ""} animated-fadeIn`}
+      className={`button ${type} ${fullWidth ? "full-width" : ""}`}
       onClick={onClick}
     >
+      {icon && <span className="button-icon">{icon}</span>}
       {children}
     </button>
   );
 };
 
 Button.propTypes = {
-  type: PropTypes.oneOf(["primary", "secondary", "tertiary"]),
+  type: PropTypes.oneOf(["primary", "secondary", "outline", "ghost"]),
   onClick: PropTypes.func,
   children: PropTypes.node.isRequired,
+  icon: PropTypes.node,
   fullWidth: PropTypes.bool,
 };
