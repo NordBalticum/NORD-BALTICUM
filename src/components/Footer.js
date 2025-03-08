b1ad6a0be292fbc74d2fe0621cdd4fd6a4d83c87
NordBalticum <info@litexus.lt>
2025-03-08 10:44:12 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,23 @@
 import React from "react";
-import "../styles/Footer.css";
+import { FaTwitter, FaTelegram, FaGithub } from "react-icons/fa";
+import "../styles/footer.css";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p className="footer-text">© 2025 NORD BALTICUM. All Rights Reserved.</p>
+        <p>© {new Date().getFullYear()} Nord Baltic. All Rights Reserved.</p>
+        <div className="social-icons">
+          <a href="https://twitter.com/nordbaltic" target="_blank" rel="noopener noreferrer">
+            <FaTwitter />
+          </a>
+          <a href="https://t.me/nordbaltic" target="_blank" rel="noopener noreferrer">
+            <FaTelegram />
+          </a>
+          <a href="https://github.com/NordBalticum" target="_blank" rel="noopener noreferrer">
+            <FaGithub />
+          </a>
+        </div>
       </div>
     </footer>
   );
