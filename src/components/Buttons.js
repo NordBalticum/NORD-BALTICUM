4f3037ad98f04d178bc043632b27aeb48163805e
NordBalticum <info@litexus.lt>
2025-03-08 17:49:32 +0200

    Update Buttons.js

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "../styles/Buttons.css";
+import "../styles/buttons.css";
 
 export const Button = ({ type = "primary", onClick, children, icon, fullWidth }) => {
   return (
