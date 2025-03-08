62dd69931c9dbceb90ea45c2dc1caa4a68539519
NordBalticum <info@litexus.lt>
2025-03-08 22:04:08 +0200

    Update admin.js

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,8 +1,7 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import { supabase } from "../utils/supabaseClient";
-import NetworkSwitcher from "../components/NetworkSwitcher";
-import "../styles/admin.css";
+import { supabase } from "@/utils/supabaseClient";
+import NetworkSwitcher from "@/components/NetworkSwitcher";
 
 export default function Admin() {
   const [users, setUsers] = useState([]);
