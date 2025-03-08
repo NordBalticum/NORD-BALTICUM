982817560814add4411e0c167060ff52cf4b7f35
NordBalticum <info@litexus.lt>
2025-03-08 18:11:28 +0200

    Update settings.js

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { supabase } from "../supabaseClient";
+import { supabase } from "../utils/supabaseClient";
 import { useAuth } from "../context/AuthContext";
 import "../styles/settings.css";
 
