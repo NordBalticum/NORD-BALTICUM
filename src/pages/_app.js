819ab6be72ff64061b5ea0851bd3231f16c5fa77
NordBalticum <info@litexus.lt>
2025-03-08 20:16:48 +0200

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,18 @@
-import "../styles/globals.css";
-import "../styles/theme.css";
-import { ThemeProvider } from "../context/ThemeContext";
-import { AuthProvider } from "../context/AuthContext";
+import "@/styles/globals.css";
+import "@/styles/theme.css";
+import { ThemeProvider } from "@/context/ThemeContext";
+import { AuthProvider } from "@/context/AuthContext";
+import Navbar from "@/components/Navbar";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  const hideNavbar = router.pathname === "/"; // Paslepia Navbar index.js puslapyje
+
   return (
     <AuthProvider>
       <ThemeProvider>
+        {!hideNavbar && <Navbar />} {/* Navbar rodomas visur, išskyrus index.js */}
         <Component {...pageProps} />
       </ThemeProvider>
     </AuthProvider>
