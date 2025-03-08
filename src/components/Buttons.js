1ed101c3e1f0f9cb77e9a9f7d69bdccbd4318591
NordBalticum <info@litexus.lt>
2025-03-08 10:27:52 +0200

    Update Buttons.js

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,9 +1,29 @@
-import "../styles/buttons.css";
+import React from "react";
+import PropTypes from "prop-types";
+import "../styles/Buttons.css";
 
-export default function Button({ text, onClick }) {
+export const PrimaryButton = ({ text, onClick }) => {
   return (
-    <button className="custom-button" onClick={onClick}>
+    <button className="button primary-button animated-fadeIn" onClick={onClick}>
       {text}
     </button>
   );
-}
+};
+
+export const SecondaryButton = ({ text, onClick }) => {
+  return (
+    <button className="button secondary-button animated-fadeIn" onClick={onClick}>
+      {text}
+    </button>
+  );
+};
+
+PrimaryButton.propTypes = {
+  text: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+SecondaryButton.propTypes = {
+  text: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
