a9d5ad61f308141e96c533d3f5d092939e279445
NordBalticum <info@litexus.lt>
2025-03-09 08:16:24 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-import styles from "../styles/index.module.css";
+import styles from "@/styles/index.module.css";
 
 export default function IndexPage() {
   const router = useRouter();
