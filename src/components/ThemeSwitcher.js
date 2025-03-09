eecd9f75f1327a4e697bda920984e04fef542ec6
NordBalticum <info@litexus.lt>
2025-03-10 01:34:46 +0200

    Update ThemeSwitcher.js

--- src/components/ThemeSwitcher.js
+++ src/components/ThemeSwitcher.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { FaSun, FaMoon } from "react-icons/fa";
-import styles from "@/styles/themeswitcher.component.css";
-import styles from "@/styles/buttons.module.css";
+import "@/styles/themeswitcher.component.css";
+import "@/styles/buttons.module.css";
 
 const ThemeSwitcher = () => {
   const [theme, setTheme] = useState(() => localStorage.getItem("theme") || "dark");
