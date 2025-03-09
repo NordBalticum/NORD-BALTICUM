2c5ee0bb8dd026132c4db96c1ded634fc6a50722
NordBalticum <info@litexus.lt>
2025-03-10 01:51:29 +0200

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -11,7 +11,6 @@ export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
       <ThemeProvider>
-        {!hideNavbar && <Navbar />}
         <Component {...pageProps} />
         <Footer />
       </ThemeProvider>
