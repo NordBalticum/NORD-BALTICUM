e4d3ccb6c0c7c14b08678dcf65274b6651ccf0d8
NordBalticum <info@litexus.lt>
2025-03-10 00:56:01 +0200

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,7 +7,6 @@ import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
-  const hideNavbar = router.pathname === "/"; // Paslepia Navbar tik index.js puslapyje
   
   return (
     <AuthProvider>
