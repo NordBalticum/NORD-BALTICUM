d11de8c0d28294850d61ef39416a65ad7e047a09
NordBalticum <info@litexus.lt>
2025-03-10 02:14:28 +0200

    Update donation.js

--- src/pages/donation.js
+++ src/pages/donation.js
@@ -3,7 +3,7 @@ import { BrowserProvider, JsonRpcProvider, formatEther, parseEther } from "ether
 import { useRouter } from "next/router";
 import { supabase } from "@/utils/supabaseClient";
 import Buttons from "@/components/Buttons"; // 🔹 Užtikrintas teisingas importas
-import "@/styles/donation.module.css";
+import styles from "@/styles/donation.module.css";
 
 const charities = [
   {
