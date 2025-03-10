bf26a2872c9bd4e977a97852f775459c2518e799
NordBalticum <info@litexus.lt>
2025-03-10 02:17:07 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { useAuth } from "@/context/AuthContext";
 import { supabase } from "@/utils/supabaseClient";
-import "@/styles/index.module.css";
+import styles from "@/styles/index.module.css";
 
 export default function Home() {
   const router = useRouter();
